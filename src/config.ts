b894ee2956a9c30d0865dbe444625be3d2613f1f
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2023-10-10 17:10:57 +0530

    add input graph and drawing board

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+const BASE_ADDR = 'http://localhost:8000/api/generate/nondim'
+const GRID_SNAP = 40
+
+/*
+load -> event to load RFP
+save -> event to send dual graph
+*/
+
+export {
+    BASE_ADDR,
+    GRID_SNAP,
+}
