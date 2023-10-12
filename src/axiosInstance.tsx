b1f4e40f60841cacd141743f3e3078a7788e6972
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2023-10-12 17:22:28 +0530

    add the api functionality and helping functions

---
+++ src/axiosInstance.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000', // Replace with your API base URL
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // Modify the request config here (e.g., add headers, authentication tokens)
+        const item = localStorage.getItem("token");
+        if (item) {
+            const accessToken = JSON.parse(item);
+
+            // ** If token is present add it to request's Authorization Header
+            if (accessToken) {
+                if (config.headers) config.headers.token = accessToken;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        // Handle request errors here
+
+        return Promise.reject(error);
+    }
+);
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+    (response) => {
+        // Modify the response data here (e.g., parse, transform)
+
+        return response;
+    },
+    (error) => {
+        // Handle response errors here
+
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
