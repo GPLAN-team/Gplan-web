faef640c88f04dc4c6ccc9b8e7e33600808fe3b0
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2024-01-07 18:47:56 +0530

    Auto stash before revert of "added ui for sidebar"

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Dashboard from "../features/inputGraphEditor/components/DashBoard";
 import LoginPage from "../features/login/LoginPage";
 import SignUpPage from "../features/login/SignUpPage";
+import Sidebar from "../features/inputGraphEditor/components/Sidebar";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/sidebar" element={<Sidebar />} />
         </Routes>
       </Router>
     </>
