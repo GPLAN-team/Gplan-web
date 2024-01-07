197e9f27da98e9263a8569d03e7d81d7b36eaaea
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2024-01-07 18:24:11 +0530

    Revert "added ui for sidebar"
    This reverts commit b76dfea39fb9f7dd7ee59c5f9c2e44270eb47113.

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,20 +2,18 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Dashboard from "../features/inputGraphEditor/components/DashBoard";
 import LoginPage from "../features/login/LoginPage";
 import SignUpPage from "../features/login/SignUpPage";
-import Sidebar from "../features/inputGraphEditor/components/Sidebar";
 
 function Home() {
   return (
-    <div style={{backgroundColor: '#1C4C8233'}}>
+    <>
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sidebar" element={<Sidebar />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
